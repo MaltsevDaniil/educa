bf3aa6e246aa4d8e2d1f03146aeaf44b825f058d
MaltsevDaniil <ge0fftwitc@gmail.com>
2025-06-12 13:05:54 +0300

    add orderField

--- courses/models.py
+++ courses/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.auth.models import User
+from .fields import OrderField
 
 class ItemBase(models.Model):
     owner = models.ForeignKey(User, related_name="%(class)s_related", on_delete=models.CASCADE)
@@ -55,15 +56,20 @@ class Module(models.Model):
                                on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
+    order = OrderField(blank=True, for_fields=["course"])
 
     class Meta:
-        ordering = ['title']
+        ordering = ['order']
 
     def __str__(self):
-        return f'{self.title}'
+        return f'{self.order}. {self.title}'
 
 class Content(models.Model):
     module = models.ForeignKey(Module, related_name='contents', on_delete=models.CASCADE)
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, limit_choices_to={'model__in':('text',"video","image","file")})
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, limit_choices_to={'model__in':('text',"video","image","filead")})
     object_id = models.PositiveIntegerField()
     item = GenericForeignKey('content_type', 'object_id')
+    order = OrderField(blank=True, for_fields=['module'])
+
+    class Meta:
+        ordering = ['order']
