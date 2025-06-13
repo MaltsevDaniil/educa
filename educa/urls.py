74124fc90587edee098d805a64bd23d0ee072ec6
MaltsevDaniil <ge0fftwitc@gmail.com>
2025-06-13 22:11:39 +0300

    add registrations for student

--- educa/urls.py
+++ educa/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     path("course/", include('courses.urls')),
     path("module/order/", views.ModuleOrderView.as_view(), name='module_order'),
     path("content/order/", views.ContentOrderView.as_view(), name='content_order'),
+    path('students/', include('students.urls')),
     path('', views.CourseListView.as_view(), name='course_list'),
 ]
 
